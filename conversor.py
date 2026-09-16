c278cc276eef6c416a28765eb42f96d94fe85a2a
Yago Fernandes de Almeida <yagofalmeida007@gmail.com>
2026-09-16 11:45:49 -0300

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,37 @@
+print("=== CONVERSOR DE TEMPERATURAS ===")
+print("1 - Celsius para Fahrenheit")
+print("2 - Fahrenheit para Celsius")
+print("3 - Celsius para Kelvin")
+print("4 - Kelvin para Celsius")
+print("5 - Fahrenheit para Kelvin")
+print("6 - Kelvin para Fahrenheit")
+
+opcao = int(input("Escolha uma opção: "))
+temperatura = float(input("Digite a temperatura: "))
+
+if opcao == 1:
+    resultado = (temperatura * 9 / 5) + 32
+    print("Resultado:", resultado, "°F")
+
+elif opcao == 2:
+    resultado = (temperatura - 32) * 5 / 9
+    print("Resultado:", resultado, "°C")
+
+elif opcao == 3:
+    resultado = temperatura + 273.15
+    print("Resultado:", resultado, "K")
+
+elif opcao == 4:
+    resultado = temperatura - 273.15
+    print("Resultado:", resultado, "°C")
+
+elif opcao == 5:
+    resultado = (temperatura - 32) * 5 / 9 + 273.15
+    print("Resultado:", resultado, "K")
+
+elif opcao == 6:
+    resultado = (temperatura - 273.15) * 9 / 5 + 32
+    print("Resultado:", resultado, "°F")
+
+else:
+    print("Opção inválida.")
