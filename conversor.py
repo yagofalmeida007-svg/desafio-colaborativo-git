faf22fe6c2359936accda78f8f17de8ededd56f4
GABRIEL OLIVEIRA MOREIRA BRAZAO <gabriel.brazao@cs.udf.edu.br>
2026-09-22 09:23:39 -0300

    Arrumei erros e fiz alterações no codigo

--- conversor.py
+++ conversor.py
@@ -1,6 +1,7 @@
 print("=== CONVERSOR DE TEMPERATURAS ===")
 
 while True:
+    print("\n--------------------------------")
     print("6 - Celsius para Fahrenheit")
     print("5 - Fahrenheit para Celsius")
     print("4 - Celsius para Kelvin")
@@ -9,37 +10,44 @@ while True:
     print("1 - Kelvin para Fahrenheit")
     print("0 - Sair")
 
-    opcao = int(input("Escolha uma opção de 1 a 6 ou 0 para sair: "))
+    opcao = int(input("Escolha uma opção: "))
 
     if opcao == 0:
         print("Programa encerrado.")
         break
 
+    if opcao < 1 or opcao > 6:
+        print("Opção inválida! Tente novamente.")
+        continue
+
     temperatura = float(input("Digite a temperatura: "))
 
+    # 1 - Kelvin para Fahrenheit
     if opcao == 1:
-        resultado = (temperatura * 9 / 5) + 32
-        print("Resultado:", resultado, "°F")
-
-    elif opcao == 2:
-        resultado = (temperatura - 32) * 5 / 9
-        print("Resultado:", resultado, "°C")
-
-    elif opcao == 3:
-        resultado = temperatura + 273.15
-        print("Resultado:", resultado, "K")
-
-    elif opcao == 4:
-        resultado = temperatura - 273.15
-        print("Resultado:", resultado, "°C")
-
-    elif opcao == 5:
-        resultado = (temperatura - 32) * 5 / 9 + 273.15
-        print("Resultado:", resultado, "K")
-
-    elif opcao == 6:
         resultado = (temperatura - 273.15) * 9 / 5 + 32
-        print("Resultado:", resultado, "°F")
+        print(f"Resultado: {resultado:.2f} °F")
 
-    else:
-        print("Opção inválida.")
+    # 2 - Fahrenheit para Kelvin
+    elif opcao == 2:
+        resultado = (temperatura - 32) * 5 / 9 + 273.15
+        print(f"Resultado: {resultado:.2f} K")
+
+    # 3 - Kelvin para Celsius
+    elif opcao == 3:
+        resultado = temperatura - 273.15
+        print(f"Resultado: {resultado:.2f} °C")
+
+    # 4 - Celsius para Kelvin
+    elif opcao == 4:
+        resultado = temperatura + 273.15
+        print(f"Resultado: {resultado:.2f} K")
+
+    # 5 - Fahrenheit para Celsius
+    elif opcao == 5:
+        resultado = (temperatura - 32) * 5 / 9
+        print(f"Resultado: {resultado:.2f} °C")
+
+    # 6 - Celsius para Fahrenheit
+    elif opcao == 6:
+        resultado = (temperatura * 9 / 5) + 32
+        print(f"Resultado: {resultado:.2f} °F")
